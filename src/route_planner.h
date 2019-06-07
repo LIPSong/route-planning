1b9136d7de2a0159f6b765b8114fa80e512bc074
宋立平(MichaelSong)-顺丰科技 <01376021@sf01376021mac.sf.com>
2019-06-08 00:39:33 +0800

    feat: finish route planning

---
+++ src/route_planner.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "route_model.h"
+
+class RoutePlanner
+{
+public:
+  RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
+  float GetDistance() const
+  {
+    return distance;
+  }
+  void AStarSearch();
+  // Add public variables or methods declarations here.
+
+private:
+  void AddNeighbors(RouteModel::Node *current_node);
+  float CalculateHValue(RouteModel::Node *node);
+  RouteModel::Node *NextNode();
+  std::vector<RouteModel::Node> ConstructFinalPath(RouteModel::Node *);
+  // Add private variables or methods declarations here.
+  std::vector<RouteModel::Node *> open_list;
+  RouteModel &m_Model;
+  RouteModel::Node *start_node;
+  RouteModel::Node *end_node;
+  float distance;
+};
